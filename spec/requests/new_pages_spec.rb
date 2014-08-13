15c2a0580d353915091cb03aa200d1b8ddc9bb25
Gorisha <gorisha92@gmail.com>
2014-08-13 12:02:19 +0530

    route changed

--- spec/requests/new_pages_spec.rb
+++ spec/requests/new_pages_spec.rb
@@ -5,7 +5,7 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      get '/dummytest/new'
+      visit '/dummytest/new'
       expect(page).to have_content('sample app')
     end
   end
