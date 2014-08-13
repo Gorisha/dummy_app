566c79cd047fe397f8d058a0487092673bd22469
Gorisha <gorisha92@gmail.com>
2014-08-13 12:10:24 +0530

    feature added

--- spec/requests/new_pages_spec.rb
+++ spec/requests/new_pages_spec.rb
@@ -1,12 +1,12 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe "Static pages" do
+# describe "Static pages" do
 
-  describe "Home page" do
+#   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit root_path
-      expect(page).to have_content('sample app')
-    end
-  end
-end
+#     it "should have the content 'Sample App'" do
+#       visit root_path
+#       expect(page).to have_content('sample app')
+#     end
+#   end
+# end
