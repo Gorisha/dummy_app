6434b91aeb21e8889f36b3f6d5f9b18f6087a4e1
Gorisha <gorisha92@gmail.com>
2014-08-13 11:57:25 +0530

    test changed

--- spec/requests/new_pages_spec.rb
+++ spec/requests/new_pages_spec.rb
@@ -4,9 +4,9 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample'" do
+    it "should have the content 'Sample App'" do
       visit '/dummytest/new'
-      expect(page).to have_content('sdfjkgkjjk')
+      expect(page).to have_content('sample app')
     end
   end
 end
