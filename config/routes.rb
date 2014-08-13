59175035fac90eb0dd402adb86f489db48bad53b
Gorisha <gorisha92@gmail.com>
2014-08-13 11:29:57 +0530

    test written

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'dummytest/new'
-
+  
+root 'dummytest#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
