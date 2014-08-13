f575ab132a685d5933dfd1ff2d64c304bf508775
Gorisha <gorisha92@gmail.com>
2014-08-13 15:57:21 +0530

    unit test for product is written

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Product do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before { @pro = Product.new(name: "Example Product", description: "example") }
+
+  subject { @pro }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:description) }
 end
+  
